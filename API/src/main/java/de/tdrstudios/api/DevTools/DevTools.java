ed69748efd30e3deaa16c1bb604d6d26ebf40b28
TDR_Minecraft <tdr.minecraft@gmail.com>
2020-07-16 13:19:17 +0200

    Add the TimeOutput Method
    You can send important things in a Frame with Time to the Console

--- API/src/main/java/de/tdrstudios/api/DevTools/DevTools.java
+++ API/src/main/java/de/tdrstudios/api/DevTools/DevTools.java
@@ -1,15 +1,20 @@
 package de.tdrstudios.api.DevTools;
 
 import de.dseelp.discordsystem.api.Discord;
+import de.dseelp.discordsystem.api.commands.CommandSender;
 import de.dseelp.discordsystem.api.commands.DiscordGuildCommandSender;
 import de.dseelp.discordsystem.utils.JsonDocument;
 import de.dseelp.modules.Module;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.TextChannel;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.text.Document;
 import java.awt.*;
+import java.time.Clock;
+import java.time.LocalDate;
+import java.util.Date;
 
 import static de.dseelp.discordsystem.api.EmbedUtils.createNormalBuilder;
 
@@ -18,9 +23,9 @@ public class DevTools {
     public static void alert(String msg, Color color, Guild guild, String title, Module module) {
         TextChannel channel = guild.getSystemChannel();
         JsonDocument doc = Discord.getGuildManager().getGuildConfig(guild).getDocument();
-        if(Discord.getGuildManager().getGuildConfig(guild).getDocument().getString("AlertChannel").isEmpty()) {
+        if (Discord.getGuildManager().getGuildConfig(guild).getDocument().getString("AlertChannel").isEmpty()) {
             System.out.println("[DevTools] A Module has send a Massage to the AlertChannel but the Channel was not setuped yet use +setup AlertChannel to fix this Bug");
-        }else {
+        } else {
             channel = (TextChannel) guild.getGuildChannelById(doc.getString("AlertChannel"));
 
         }
@@ -30,10 +35,6 @@ public class DevTools {
         eb.setColor(color);
         eb.setFooter("This Text was send by " + module.getName());
         channel.sendMessage(eb.build()).queue();
-
-
-
-
 
 
     }
@@ -46,4 +47,23 @@ public class DevTools {
         channel.sendMessage(eb.build()).queue();
 
     }
+
+        public static void TimeOutput(String msg, Module module) {
+            Date date = new Date();
+            System.out.println("-------------------------------------");
+            System.out.println(date.toString());
+            System.out.println(msg + "send by " + module.getName());
+            System.out.println("-------------------------------------");
+        }
+         public static void TimeOutput(String msg) {
+        Date date = new Date();
+        System.out.println("-------------------------------------");
+        System.out.println(date.toString());
+        System.out.println(msg);
+        System.out.println("-------------------------------------");
+         }
+
+
+
 }
+
---
+++ Core/src/main/java/de/tdrstudios/discordsystem/core/Executeable.java
@@ -0,0 +1,17 @@
+package de.tdrstudios.discordsystem.core;
+
+
+import de.tdrstudios.api.DevTools.DevTools;
+import de.tdrstudios.utils.Branding;
+
+public class Executeable {
+
+    public static void main(String[] args) {
+        System.out.println(Branding.Big1.getBranding());
+        DevTools.TimeOutput("Test");
+
+        
+
+
+    }
+}

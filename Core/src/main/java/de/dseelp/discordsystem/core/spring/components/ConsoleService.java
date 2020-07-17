988f9a012d81940592890c48201ca05ce9f4da99
DSeeLP <dseelp@gmail.com>
2020-07-17 11:51:36 +0200

    Fixed Conflict

--- Core/src/main/java/de/dseelp/discordsystem/core/spring/components/ConsoleService.java
+++ Core/src/main/java/de/dseelp/discordsystem/core/spring/components/ConsoleService.java
@@ -29,26 +29,12 @@ public class ConsoleService {
     public void start() {
         System.out.println("Creating");
         console = ConsoleInitializer.initialize("Console > ", "main");
-<<<<<<< HEAD
-        console.addReadHandler("commandHandler", new Consumer<String>() {
-            @Override
-            public void accept(String s) {
-                system.execute(new ConsoleCommandSender(), system.parseCommand(s));
-            }
-        });
-        LoggerRegistry.register("normal", ConsoleSystem.createSubLogger(LoggerRegistry.get().getLogger(), "System"));
-        LogSystem normalLogger = LoggerRegistry.get("normal");
-        System.setOut(normalLogger.getOut());
-        System.setErr(normalLogger.getError());
-
-=======
         LoggerRegistry.register("normal", ConsoleSystem.createSubLogger(LoggerRegistry.get().getLogger(), "DiscordSystem"));
         LoggerRegistry.register("modules", ConsoleSystem.createSubLogger(LoggerRegistry.get("normal").getLogger(), "Modules"));
         LogSystem normalLogger = LoggerRegistry.get("normal");
         System.setOut(normalLogger.getOut());
         System.setErr(normalLogger.getError());
         Discord.setCommandSystem(new CommandSystem());
->>>>>>> 5a1be0de0753e1ef0fdbf29190b15f387b0cdfd6
     }
 
     public void stop() {

f085b37c65f29d7fdb942c0354509a26b66285a1
TDR_Minecraft <tdr.minecraft@gmail.com>
2020-08-09 09:50:10 +0200

    Way to Launcher

--- Launcher/src/main/java/de/dseelp/discordsystem/launcher/LauncherMain.java
+++ Launcher/src/main/java/de/dseelp/discordsystem/launcher/LauncherMain.java
@@ -1,7 +1,55 @@
 package de.dseelp.discordsystem.launcher;
 
+import lombok.Getter;
+
 public class LauncherMain {
+
+    @Getter
+    public boolean GUIActive;
     public static void main(String[] args) {
+
+
+
+    }
+
+    public boolean StartGUI() {
+        System.out.println("[Launcher] init!");
+
+        if(GUIActive == true) {
+
+
+            System.err.println("[Launcher] Cant start Laucher! -> Launcher is alredy active!");
+            return false;
+
+        }else {
+            //Start the Launcher!
+            return true;
+        }
+
+    }
+
+    public boolean GUIStatus() {
+
+        if(GUIActive) {
+            return true;
+        }else {
+            return false;
+        }
+
+
+
+    }
+
+    public boolean StopGUI() {
+
+        if(GUIActive) {
+            //Stop the GUI
+            return true;
+        } else {
+
+            System.err.println("Cant Stop the GUI! -> Gui isnt Aktive!");
+            return false;
+        }
 
     }
 }

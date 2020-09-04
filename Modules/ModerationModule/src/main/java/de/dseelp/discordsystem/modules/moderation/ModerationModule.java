936901713cedf08c923f1378584a6198aa39c017
DSeeLPYT <dseelp@gmail.com>
2020-09-04 17:48:48 +0200

    Added ModerationModule

---
+++ Modules/ModerationModule/src/main/java/de/dseelp/discordsystem/modules/moderation/ModerationModule.java
@@ -0,0 +1,19 @@
+package de.dseelp.discordsystem.modules.moderation;
+
+import de.dseelp.discordsystem.api.DiscordModule;
+import de.dseelp.discordsystem.api.modules.NewModule;
+
+@NewModule(name = "Moderation", authors = "DSeeLP", version = "0.2-ALPHA")
+public class ModerationModule extends DiscordModule {
+    @Override
+    public void onEnable() {
+
+    }
+
+
+
+
+    public void registerCommands() {
+
+    }
+}
